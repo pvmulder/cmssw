0aef40d48b0953b7681a6e02c7c9e7c8885fad24
Petra <pvmulder@cern.ch>
2013-12-05 10:30:55 +0100

    fix typo

--- RecoBTau/JetTagMVALearning/test/CSV_MLP_newVars/doTraining.sh
+++ RecoBTau/JetTagMVALearning/test/CSV_MLP_newVars/doTraining.sh
@@ -17,31 +17,31 @@ Combinations="NoVertex_B_DUSG NoVertex_B_C PseudoVertex_B_DUSG PseudoVertex_B_C 
 CAT="Reco Pseudo No"
 prefix="CombinedSV"
 
-echo "Filling the 2D pt/eta histograms" 
+#echo "Filling the 2D pt/eta histograms" 
 
-g++ ./histoJetEtaPt.cpp `root-config --cflags --glibs` -o histos
-./histos $path_to_rootfiles $prefix
+#g++ ./histoJetEtaPt.cpp `root-config --cflags --glibs` -o histos
+#./histos $path_to_rootfiles $prefix
 
-echo "Reweighting the trees according to the pt/eta weights and saving the relevant variables " 
+#echo "Reweighting the trees according to the pt/eta weights and saving the relevant variables " 
 
-files=("MVATrainer_No_B_DUSG_cfg.py" "MVATrainer_No_B_C_cfg.py" "MVATrainer_Pseudo_B_DUSG_cfg.py" "MVATrainer_Pseudo_B_C_cfg.py" "MVATrainer_Reco_B_DUSG_cfg.py" "MVATrainer_Reco_B_C_cfg.py")
-
-l=0
-while [ $l -lt 6 ]
-do
-	jobsrunning=0
-	while [ $jobsrunning -lt $answer ]
-	do
-echo ${files[l]}
-		nohup cmsRun ${files[l]} &
-		let jobsrunning=$jobsrunning+1
-		let l=$l+1
-	done
-	wait
-done
-
-
-echo "Do the actual training"
+#files=("MVATrainer_No_B_DUSG_cfg.py" "MVATrainer_No_B_C_cfg.py" "MVATrainer_Pseudo_B_DUSG_cfg.py" "MVATrainer_Pseudo_B_C_cfg.py" "MVATrainer_Reco_B_DUSG_cfg.py" "MVATrainer_Reco_B_C_cfg.py")
+#
+#l=0
+#while [ $l -lt 6 ]
+#do
+#	jobsrunning=0
+#	while [ $jobsrunning -lt $answer ]
+#	do
+#echo ${files[l]}
+#		nohup cmsRun ${files[l]} &
+#		let jobsrunning=$jobsrunning+1
+#		let l=$l+1
+#	done
+#	wait
+#done
+#
+#
+#echo "Do the actual training"
 
 CombinationsArray=("NoVertex_B_DUSG" "NoVertex_B_C" "PseudoVertex_B_DUSG" "PseudoVertex_B_C" "RecoVertex_B_DUSG" "RecoVertex_B_C")
 l=0
@@ -54,7 +54,7 @@ do
  		mkdir tmp${CombinationsArray[l]}
  		cd tmp${CombinationsArray[l]}
 		echo mvaTreeTrainer ../Train_${CombinationsArray[l]}.xml tmp.mva ../train_${CombinationsArray[l]}_save.root
-		nohup mvaTreeTrainer ../Train_${CombinationsArray[l]}B.xml tmp.mva ../train_${CombinationsArray[l]}_save.root &
+		nohup mvaTreeTrainer ../Train_${CombinationsArray[l]}.xml tmp.mva ../train_${CombinationsArray[l]}_save.root &
 		cd ..
 		let jobsrunning=$jobsrunning+1
 		let l=$l+1
