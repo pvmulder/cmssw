1e5383353603facc2e0240e1803d66b764265b1c
Petra <pvmulder@cern.ch>
2013-12-05 12:56:15 +0100

    Variable extraction for all vars (including new ones) + also IVF

--- RecoBTau/JetTagMVALearning/test/CSV_MLP_newVars/VariableExtraction/VariableExtractor_IVF_cfg.py
+++ RecoBTau/JetTagMVALearning/test/CSV_MLP_newVars/VariableExtraction/VariableExtractor_IVF_cfg.py
@@ -7,7 +7,7 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
 process.load("RecoBTau.JetTagComputer.jetTagRecord_cfi")
 process.load("RecoBTag.SecondaryVertex.combinedSecondaryVertexES_cfi")
-
+#process.combinedSecondaryVertex.trackMultiplicityMin = cms.uint32(2)
 
 # load the full reconstraction configuration, to make sure we're getting all needed dependencies
 process.load("Configuration.StandardSequences.MagneticField_cff")
@@ -33,9 +33,14 @@ process.GlobalTag.globaltag = cms.string("START53_V26::All")
 #)
 #process.es_prefer_BTauMVAJetTagComputerRecord = cms.ESPrefer("PoolDBESSource","BTauMVAJetTagComputerRecord")
 
+#for Inclusive Vertex Finder
+process.load('RecoVertex/AdaptiveVertexFinder/inclusiveVertexing_cff')
+process.load('RecoBTag/SecondaryVertex/inclusiveSecondaryVertexFinderTagInfos_cfi')
+
 #define you jet ID
 jetID = cms.InputTag("ak5PFJets")
 JetCut=cms.string("neutralHadronEnergyFraction < 0.99 && neutralEmEnergyFraction < 0.99 && nConstituents > 1 && chargedHadronEnergyFraction > 0.0 && chargedMultiplicity > 0.0 && chargedEmEnergyFraction < 0.99")
+
 
 #do the PFnoPU using PF2PAT
 process.out = cms.OutputModule("PoolOutputModule",
@@ -46,7 +51,6 @@ process.load("PhysicsTools.PatAlgos.patSequences_cff")
 from PhysicsTools.PatAlgos.tools.pfTools import *
 postfix="PF2PAT"
 usePF2PAT(process,runPF2PAT=True, jetAlgo="AK5", runOnMC=True, postfix=postfix, pvCollection=cms.InputTag('goodOfflinePrimaryVertices'), typeIMetCorrections=False
-#,jetCorrections=('AK5PFchs', ['L1FastJet','L2Relative','L3Absolute']), pvCollection=cms.InputTag('goodOfflinePrimaryVertices'), typeIMetCorrections=False, outputModules=['out']
 )
 process.patJetCorrFactorsPF2PAT.payload = 'AK5PFchs'
 process.patJetCorrFactorsPF2PAT.levels = cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute'])
@@ -54,7 +58,7 @@ process.pfPileUpPF2PAT.checkClosestZVertex = False
 process.patJetsPF2PAT.discriminatorSources = cms.VInputTag(
         cms.InputTag("trackCountingHighEffBJetTagsAODPF2PAT")
 )
-process.btaggingJetTagsAODPF2PAT = cms.Sequence(getattr(process,"trackCountingHighEffBJetTagsAODPF2PAT") )
+process.btaggingJetTagsAODPF2PAT = cms.Sequence(getattr(process,"combinedSecondaryVertexBJetTagsAODPF2PAT") )
 # top projections in PF2PAT:
 getattr(process,"pfNoPileUp"+postfix).enable = True
 #applyPostfix(process,"patJetCorrFactors",postfix).payload = cms.string('AK5PFchs')
@@ -69,7 +73,7 @@ newjetID=cms.InputTag("selectedPatJetsPF2PAT")
 from RecoJets.JetAssociationProducers.j2tParametersVX_cfi import *
 process.myak5JetTracksAssociatorAtVertex = cms.EDProducer("JetTracksAssociatorAtVertex",
                                                   j2tParametersVX,
-                                                  jets = jetID
+                                                  jets = jetID # replaced by newjetID later
                                                   )
 
 #new input for impactParameterTagInfos
@@ -77,13 +81,14 @@ from RecoBTag.Configuration.RecoBTag_cff import *
 process.impactParameterTagInfos.jetTracks = cms.InputTag("myak5JetTracksAssociatorAtVertex")
 
 process.load("PhysicsTools.JetMCAlgos.CaloJetsMCFlavour_cfi")  
-process.AK5byRef.jets = jetID
+process.AK5byRef.jets = jetID # replaced by newjetID later
 
 #do the matching
 process.flavourSeq = cms.Sequence(
     process.myPartons *
     process.AK5Flavour
     )
+
 
 #select good primary vertex
 from PhysicsTools.SelectorUtils.pvSelector_cfi import pvSelector
@@ -97,27 +102,27 @@ process.myak5JetTracksAssociatorAtVertex.jets = newjetID
 process.AK5byRef.jets                         = newjetID
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(10000)
+    input = cms.untracked.int32(100)
 )
 
 process.source = cms.Source("PoolSource",
 #	fileNames = cms.untracked.vstring('file:testTagInfos.root')
 	fileNames = cms.untracked.vstring(
-'/store/mc/Summer12_DR53X/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola/AODSIM/PU_S10_START53_V7A-v1/0000/001C868B-B2E1-E111-9BE3-003048D4DCD8.root',
-'/store/mc/Summer12_DR53X/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola/AODSIM/PU_S10_START53_V7A-v1/0000/0046E17E-BCE1-E111-A1D1-003048F02CB2.root',
-'/store/mc/Summer12_DR53X/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola/AODSIM/PU_S10_START53_V7A-v1/0000/0076C8E3-9AE1-E111-917C-003048D439AA.root',
-'/store/mc/Summer12_DR53X/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola/AODSIM/PU_S10_START53_V7A-v1/0000/0244AEA1-7CE1-E111-956B-0025901D4C3C.root',
-'/store/mc/Summer12_DR53X/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola/AODSIM/PU_S10_START53_V7A-v1/0000/024A37B1-C9E1-E111-9CDF-0025901D4B04.root',
-'/store/mc/Summer12_DR53X/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola/AODSIM/PU_S10_START53_V7A-v1/0000/027E3BA0-F2E1-E111-AAD3-003048D3CA06.root',
-'/store/mc/Summer12_DR53X/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola/AODSIM/PU_S10_START53_V7A-v1/0000/02963472-F6E1-E111-B0F9-0030487D814D.root',
-'/store/mc/Summer12_DR53X/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola/AODSIM/PU_S10_START53_V7A-v1/0000/02A13705-B0E1-E111-8248-0030487E4EB5.root',
-'/store/mc/Summer12_DR53X/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola/AODSIM/PU_S10_START53_V7A-v1/0000/02A9D360-C9E1-E111-B4AE-003048D3CA06.root',
-'/store/mc/Summer12_DR53X/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola/AODSIM/PU_S10_START53_V7A-v1/0000/02F5A838-8FE1-E111-B0C8-00266CFFA654.root',
-'/store/mc/Summer12_DR53X/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola/AODSIM/PU_S10_START53_V7A-v1/0000/02FDED20-B9E1-E111-B985-0030487D5EBD.root',
-'/store/mc/Summer12_DR53X/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola/AODSIM/PU_S10_START53_V7A-v1/0000/040B9556-C2E1-E111-A7F7-00266CF1074C.root',
-
+#'/store/mc/Fall11/QCD_Pt-50to80_Tune4C_7TeV_pythia8/AODSIM/PU_S6_START44_V9B-v1/0000/00225C27-4744-E111-8583-003048C692E4.root',
+#'/store/mc/Fall11/QCD_Pt-80to120_Tune4C_7TeV_pythia8/AODSIM/PU_S6_START44_V9B-v1/0000/00D668C4-5244-E111-A8F6-0025901D4936.root',
+#'/store/mc/Fall11/QCD_Pt-120to170_Tune4C_7TeV_pythia8/AODSIM/PU_S6_START44_V9B-v1/0000/023CE3AD-CF46-E111-90CB-003048C690A0.root',
+#'/store/mc/Fall11/QCD_Pt-170to300_Tune4C_7TeV_pythia8/AODSIM/PU_S6_START44_V9B-v1/0000/0433579C-7644-E111-9D93-00215AD4D6C8.root',
+#'/store/mc/Fall11/QCD_Pt-300to470_Tune4C_7TeV_pythia8/AODSIM/PU_S6_START44_V9B-v1/0000/00C128DE-923F-E111-AA76-003048C69312.root',
+#'/store/mc/Fall11/QCD_Pt-470to600_Tune4C_7TeV_pythia8/AODSIM/PU_S6_START44_V9B-v1/0000/00CC4BA4-FC45-E111-A993-003048F0E80C.root',
+#'/store/mc/Fall11/QCD_Pt-600to800_Tune4C_7TeV_pythia8/AODSIM/PU_S6_START44_V9B-v1/0000/000548D5-0A40-E111-A327-002481E0DA4E.root',
+#'/store/mc/Fall11/QCD_Pt-800to1000_Tune4C_7TeV_pythia8/AODSIM/PU_S6_START44_V9B-v1/0000/0469BF65-8343-E111-8E43-003048F0E3B2.root',
+#'/store/mc/Fall11/QCD_Pt-1000to1400_Tune4C_7TeV_pythia8/AODSIM/PU_S6_START44_V9B-v1/0000/14151191-9043-E111-BA90-003048C690A0.root',
+#'/store/mc/Fall11/QCD_Pt-1400to1800_Tune4C_7TeV_pythia8/AODSIM/PU_S6_START44_V9B-v1/0000/0E10C938-1146-E111-95AC-0025901D4A58.root',
+#'/store/mc/Fall11/QCD_Pt-1800_Tune4C_7TeV_pythia8/AODSIM/PU_S6_START44_V9B-v1/0000/0C4D9605-2544-E111-B39F-003048D4DF80.root',
+'/store/mc/Summer12_DR53X/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola/AODSIM/PU_S10_START53_V7A-v1/0000/FED775BD-B8E1-E111-8ED5-003048C69036.root'
 	)
 )
+
 
 process.combinedSVMVATrainer = cms.EDAnalyzer("JetTagMVAExtractor",
 	variables = cms.untracked.VPSet(
@@ -132,12 +137,13 @@ process.combinedSVMVATrainer = cms.EDAnalyzer("JetTagMVAExtractor",
 "jetPt","jetEta","vertexCategory","trackSip2dSig","trackSip3dSig","trackSip2dVal","trackSip3dVal","trackPtRel","trackPPar","trackDeltaR","trackPtRatio","trackPParRatio","trackJetDist","trackDecayLenVal","trackSip2dSigAboveCharm","trackSip3dSigAboveCharm","trackSumJetEtRatio","trackSumJetDeltaR","jetNTracks","trackSip2dValAboveCharm","trackSip3dValAboveCharm","chargedHadronEnergyFraction","neutralHadronEnergyFraction","photonEnergyFraction","electronEnergyFraction","muonEnergyFraction","chargedHadronMultiplicity","neutralHadronMultiplicity","photonMultiplicity","electronMultiplicity","muonMultiplicity","hadronMultiplicity","hadronPhotonMultiplicity","totalMultiplicity"
 )) # no trackEtaRel!!!???!!!
 
-
 	),
 	ipTagInfos = cms.InputTag("impactParameterTagInfos"),
-	svTagInfos =cms.InputTag("secondaryVertexTagInfos"),
+	#svTagInfos =cms.InputTag("secondaryVertexTagInfos"),
+	svTagInfos =cms.InputTag("inclusiveSecondaryVertexFinderTagInfos"),
 	
-	minimumTransverseMomentum = cms.double(10.0),
+	minimumTransverseMomentum = cms.double(15.0),
+	maximumTransverseMomentum = cms.double(9999999.),
 	useCategories = cms.bool(True),
         calibrationRecords = cms.vstring(
                 'CombinedSVRecoVertex',
@@ -155,10 +161,13 @@ process.combinedSVMVATrainer = cms.EDAnalyzer("JetTagMVAExtractor",
 process.p = cms.Path(
 process.goodOfflinePrimaryVertices * 
 process.JECAlgo * 
+process.inclusiveVertexing * 
+#process.inclusiveMergedVerticesFiltered * 
+#process.bToCharmDecayVertexMerged * 
 process.myak5JetTracksAssociatorAtVertex * 
 process.impactParameterTagInfos * 
-process.secondaryVertexTagInfos * 
-process.flavourSeq *  
+process.inclusiveSecondaryVertexFinderTagInfos *
+process.flavourSeq * 
 process.combinedSVMVATrainer 
 )
 
